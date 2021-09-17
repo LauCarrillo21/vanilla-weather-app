5b8f033df06828731e5b911ef8b606bcbc2ddcd6
Laura <laucarrillo21@gmail.com>
2021-09-17 17:18:35 -0500

    Forecast API

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate (timestamp) {
     return `${day} ${hours}:${minutes} `;
     }
 
-function displayForecast () {
+function displayForecast (response) {
+    console.log (response.data.daily);
     let forecastElement = document.querySelector ("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -51,6 +52,13 @@ function displayForecast () {
     forecastElement.innerHTML = forecastHTML;  
 }
 
+function  getForecast (coordinates) {
+    console.log (coordinates);
+    let apiKey = "7c0a7bf7120da0d635ff53ea1528f155";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 
 function displayTemperature (response) {
     let temperatureElement = document.querySelector ("#temperature");
@@ -70,6 +78,8 @@ function displayTemperature (response) {
     dateElement.innerHTML = formatDate (response.data.dt * 1000); 
     iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute ("alt", response.data.weather[0].description)
+
+    getForecast (response.data.coord);
 }
 
 
@@ -106,7 +116,7 @@ function displayCelsiusTemperature (event) {
 }
 
 let celsiusTemperature = null;
-displayForecast ();
+
 
 search ("New York");
 
